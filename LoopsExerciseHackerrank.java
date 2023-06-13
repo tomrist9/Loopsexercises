f9bfc2d660251256bb68e69650eb8977e6ba987c
Mehmet Elibeyli <tomristt9@gmail.com>
2023-06-13 15:58:02 +0400

    LoopsExerciseHackerrank.java

---
+++ LoopsExerciseHackerrank.java
@@ -0,0 +1,16 @@
+public class LoopsExerciseHackerrank {
+
+    public static void main(String[] args) {
+
+        int N = 2;
+
+
+        int multiplesCount =10;
+
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+    }
+}
+}
